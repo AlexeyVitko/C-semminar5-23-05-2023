2d82994b8cf7896af44ce690f5bf6401ae871636
AlexVi <vpa24082012@gmal.com>
2023-05-29 22:18:11 +0300

    Добавили решения задач 46

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+  кому  Все
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+ool NumberInArray (int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine ("Введите число элементов");
+int elements = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [elements];
+
+Console.WriteLine ("Введите минимальное число диапазона");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите максимальное число диапазона");
+int maximum = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(elements, minimum, maximum+1);
+
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+Console.WriteLine ("Введите число, которое нужно проверить в массиве");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isExistNum = NumberInArray (number, array);
+Console.WriteLine (isExistNum ? "Число есть в массиве": "Числа нет в массиве");
+
+
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+bool NumberInArray (int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
+
+Console.WriteLine ("Введите число элементов");
+int elements = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите минимальное число диапазона");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите максимальное число диапазона");
+int maximum = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(elements, minimum, maximum);
+
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+Console.WriteLine ("Введите число, которое нужно проверить в массиве");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isExistNum = NumberInArray (number, array);
+Console.WriteLine (isExistNum ? "Число есть в массиве": "Числа нет в массиве");
+
+
+
+
+
+Домашка 
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+void PrintArrayDouble(double[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
